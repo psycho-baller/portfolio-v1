00fd72e88d97524fc291dc28a9176a5a7517fbc5
psycho-baller <Ramim66809@gmail.com>
2022-08-27 12:41:16 +0400

    added link to V1.0 and changed package

--- components/infoButton.tsx
+++ components/infoButton.tsx
@@ -26,8 +26,21 @@ const InfoButton = () => {
           <PopoverArrow />
           <PopoverCloseButton />
           <PopoverHeader>Hey!</PopoverHeader>
-          <PopoverBody>This is my boook themed personal/portfolio website. If you have any suggestions on what I could do to improve my website, please feel free to contact me through the contact form at the end of the book or through email or linkedIn (links are in the cover page)</PopoverBody>
-          <PopoverFooter>V1.0</PopoverFooter>
+          <PopoverBody>
+            This is my boook themed personal/portfolio website. If you have any
+            suggestions on what I could do to improve my website, please feel
+            free to contact me through the contact form at the end of the book
+            or through email or linkedIn (links are in the cover page)
+          </PopoverBody>
+          <PopoverFooter>
+            <a
+              href="https://github.com/psycho-baller/portfolio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              V1.0
+            </a>
+          </PopoverFooter>
         </PopoverContent>
       </Popover>
     );
